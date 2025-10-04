11309e7a8e0dd8f7feb4369c99e94b96e7ccad17
PrimeGoose <64712362+PrimeGoose@users.noreply.github.com>
2025-10-04 12:06:35 +0200

    CHANNEL = "kalnins"   # Kick channel

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from playwright.async_api import async_playwright
 import subprocess
 import time
 
-CHANNEL = "trainwreckstv"   # Kick channel
+CHANNEL = "kalnins"   # Kick channel
 CHECK_INTERVAL = 60         # seconds between checks
 YT_DLP_PATH = "yt-dlp"      # assumes yt-dlp installed in venv
 
